4d04ec862e720afdadf7228a44ceb2e63db09685
alitemel89 <alitemel89.ce@gmail.com>
2021-10-13 10:01:07 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to Memories API');
 })
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
